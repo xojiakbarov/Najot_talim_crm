b58c05135e8416a82cd7eff760a2a2cab41bea98
mahmudxon1 <abdumutalibov.xon@gmail.com>
2023-09-28 22:06:00 +0500

    Hammasi biti qib o'tirmeylar

--- bin/main.dart
+++ bin/main.dart
@@ -1,36 +1,71 @@
 import 'dart:io';
-
 import 'najot_talim_crm.dart';
 
+ String cfirstName="";
+ String clastName="";
+ String cpassword="";
+
+welcome_page() {
+  print("""
+      1 - login 
+      2 - sign in
+      3 - exit
+      4 - logout
+      """);
+  int choice =int.parse(stdin.readLineSync()!);
+  int userInput = choice;
+  if (userInput == 1) {
+    login_page();
+  } else if (userInput == 2) {
+    signin_page();
+  } else if (userInput == 3) {
+    exit(0);
+  } else if(userInput == 4) {
+    logout_page();
+
+  }
+}
+
+logout_page() {
+  print("Siz chiqib ketingiz");
+}
+login_page() {
+    print("Login");
+  print("Ismizi kiriting: ");
+  String? firstName=stdin.readLineSync();
+  print("Familiyangizi kiriting: ");
+  String? lastName=stdin.readLineSync();
+  print("password: ");
+    String? password=stdin.readLineSync();
+
+  if(cfirstName == firstName || clastName == lastName || cpassword == password){
+    print("Logindan o'tdingiz");
+}else{
+    print("Login hato");
+  }
+}
+
+signin_page() {
+  print("Sing in");
+  print("Ismizi kiriting: ");
+  String? cfirstName=stdin.readLineSync();
+  print("Familiyangizi kiriting: ");
+  String? clastName=stdin.readLineSync();
+  print("password: ");
+  String? cpassword=stdin.readLineSync();
+  print("Royhatdan o'tiz");
+
+}
 
 void main() {
-  Student student = Student("Eshmat", "Toshmatov", 1111);
+welcome_page();
+  // Student student = Student("Eshmat", "Toshmatov", 1111);
   // print(student.get_password());
   // student.set_password(1211, 2222);
   // print(student.get_password());
 
 
 
-//   print("Login");
-//   print("Ismizi kiriting: ");
-//   String? firstName=stdin.readLineSync();
-//   print("Familiyangizi kiriting: ");
-//   String? lastName=stdin.readLineSync();
-//   print("password: ");
-//   int password=int.parse(stdin.readLineSync()!);
-//   print("Siz royhatdan o'tingiz");
-//   print("Sing in");
-//   print("Ismizi kiriting: ");
-//   String? firstName1=stdin.readLineSync();
-//   print("Familiyangizi kiriting: ");
-//   String? lastName1=stdin.readLineSync();
-//   print("password: ");
-//   int password1=int.parse(stdin.readLineSync()!);
-//
-//   if(firstName1 == firstName || lastName1 == lastName || password1 == password){
-//     print("Muvavffaqiyatli o'tdingiz");
-// }else{
-//     print("Login hato");
-//   }
+
 }
 

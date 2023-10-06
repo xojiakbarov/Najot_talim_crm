62c83af6c34df695dbd10b2793856e8c7844ab7b
Xoji <xojixojiakbarov6@gmail.com>
2023-10-06 05:14:19 +0500

    Print funksiyasi qo'shildi va print funksiyasi uchun alohida menyu chiqadi

--- lib/functions/list_of_Students.dart
+++ lib/functions/list_of_Students.dart
@@ -1,24 +1,67 @@
+import 'package:najot_talim_crm/functions/validator.dart';
 import 'package:najot_talim_crm/najot_talim_crm.dart';
 
 void listOfStudents() {
   print("kirvotti");
-  var students = repository.fetchStudents();
-  var teachers = teacherRepository.fetchTeachers();
-
-  if (students.isEmpty ) {
-    for (final student in students) {
-      print(student);
-    }
-   
-  } else if (teachers.isEmpty){
-      for (final teacher in teachers) {
-            print(teacher);
-          }
-  } else {
+    bool isTerminated = true;
+  do {
     print("""
+<<< Kimni ro'yxatni ko'rishni istaysiz? >>>
 
-Hali hech kim  ro'yxatdan o'tmagan. Avval ro'yxatdan o'ting!
-
+        1. O'qituvchi
+        2. Talaba
+        0. Orqaga
+        
 """);
-  }
+    var input = validator("Buyruq");
+    var option = int.tryParse(input);
+    if (option != null) {
+      if (option >= 0 && option <= 2) {
+        switch (option) {
+          case 0:
+            isTerminated = true;
+            break;
+          case 1:
+            var teachers = teacherRepository.fetchTeachers();
+            for(final teacher in teachers) {
+              print(teacher);
+            }
+            break;
+          case 2:
+            var students = repository.fetchStudents();
+            for( final student in students) {
+              print(student);
+            }
+            break;
+        }
+      } else {
+        print("Bunday buyruq mavjud emas");
+      }
+    } else {
+      print("Yaroqsiz qiymat kiritildi");
+    }
+  } while (!isTerminated);
+  
 }
+
+
+
+// var students = repository.fetchStudents();
+//   var teachers = teacherRepository.fetchTeachers();
+
+//   if (students.isEmpty ) {
+//     for (final student in students) {
+//       print(student);
+//     }
+   
+//   } else if (teachers.isEmpty){
+//       for (final teacher in teachers) {
+//             print(teacher);
+//           }
+//   } else {
+//     print("""
+
+// Hali hech kim  ro'yxatdan o'tmagan. Avval ro'yxatdan o'ting!
+
+// """);
+//   }
